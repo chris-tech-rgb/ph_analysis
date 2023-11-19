355c11b67eae8d651a14e5349d7378785a92751c
chris-tech-rgb <fatdasdavidson755@gmail.com>
2023-11-19 13:43:28 +0900

    plot_img_and_hist

--- startup.py
+++ startup.py
@@ -1,5 +1,45 @@
 import os
+import skimage as ski
+
 from natsort import natsorted, ns
+
+
+matplotlib.rcParams['font.size'] = 8
+
+
+def plot_img_and_hist(image, axes, bins=256):
+    """Plot an image along with its histogram and cumulative histogram.
+
+    """
+    image = img_as_float(image)
+    ax_img, ax_hist = axes
+    ax_cdf = ax_hist.twinx()
+
+    # Display image
+    ax_img.imshow(image, cmap=plt.cm.gray)
+    ax_img.set_axis_off()
+
+    # Display histogram
+    ax_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
+    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+    ax_hist.set_xlabel('Pixel intensity')
+    ax_hist.set_xlim(0, 1)
+    ax_hist.set_yticks([])
+
+    # Display cumulative distribution
+    img_cdf, bins = exposure.cumulative_distribution(image, bins)
+    ax_cdf.plot(bins, img_cdf, 'r')
+    ax_cdf.set_yticks([])
+
+    return ax_img, ax_hist, ax_cdf
+
+
+# Load example images
 folder_path = os.path.join(os.getcwd(), 'images')
 list_files = os.listdir(folder_path)
+list_files = natsorted(list_files)
 print(list_files)
+image_list = []
+for filename in list_files:
+  filename = os.path.join(folder_path, filename)
+  image_list.append(ski.io.imread(filename))
