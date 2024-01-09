0299eced5cf2193cec46ce60e60cfc2486e3ed07
chris-tech-rgb <fatdasdavidson755@gmail.com>
2024-01-09 22:22:02 +0900

    1

--- calibration_curve.py
+++ calibration_curve.py
@@ -4,8 +4,7 @@ import numpy as np
  
 # Fitting function
 def func(rgb, a1, a2, b1, b2, c1, c2):
-    return a1*rgb[:, 0]**a2+b1*rgb[:, 1]**b2+c1*rgb[:, 2]**c2
-    #return a1*r**a2+b1*g**b2+c1*b**c2
+    return a1 * rgb[:, 0]**a2 + b1 * rgb[:, 1]**b2 + c1 * rgb[:, 2]**c2
  
 # Experimental data points    
 pH_values = np.array([4.65, 5.50, 5.95, 6.40, 6.70, 7.00, 7.60, 8.00])
@@ -17,28 +16,34 @@ rgb_values = np.array([[146.29, 135.73, 50.21],
                        [28.64, 71.22, 72.75],
                        [26.41, 60.29, 85.66],
                        [16.36, 42.82, 73.87]])
+r = rgb_values[:, 0]
+g = rgb_values[:, 1]
+b = rgb_values[:, 2]
 
 # Perform the curve-fit
 popt, pcov = curve_fit(func, rgb_values, pH_values, maxfev = 20000)
 a1, a2, b1, b2, c1, c2 = popt
-print("Calibration Curves:\npH = "+str(a1)+" × R^("+str(a2)+") + "+str(b1)+" × G^("+str(b2)+") + "+str(c1)+" × B^("+str(c2)+")")
-print(func(rgb_values, a1, a2, b1, b2, c1, c2))
+print("Calibration Curves:\npH = " + str(a1) + " × R^(" + str(a2) + ") + " + str(b1) + " × G^(" + str(b2) + ") + " + str(c1) + " × B^(" + str(c2) + ")")
 
 # Create figure
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Set values
-r = rgb_values[:, 0]
-g = rgb_values[:, 1]
-b = rgb_values[:, 2]
-ph = pH_values
+# Fitting
+r_fit = np.linspace(min(r), max(r), 16)
+g_fit = np.linspace(min(g), max(g), 16)
+b_fit = np.linspace(min(b), max(b), 16)
+R, G, B = np.meshgrid(r_fit, g_fit, b_fit)
+pH = a1 * R**a2 + b1 * G**b2 + c1 * B**c2
 
-# Create colorbar figure
-img = ax.scatter(r, g, b, c=ph, cmap='viridis_r')
-cb = fig.colorbar(img, location = 'left')
+# Scatter plot
+img = ax.scatter(R, G, B, c=pH, cmap='viridis_r')
+
+# Colorbar
+colorbar = fig.colorbar(img, location='left')  # Set position to 'left'
+colorbar.set_label('pH Values')
+
 # Set labels
-cb.set_label('pH')
 ax.set_xlabel('R')
 ax.set_ylabel('G')
 ax.set_zlabel('B')
