44a9ecb7b51361a6df46e77c3f7f14d666d12362
cyl <afgugewufv@gmail.com>
2024-05-24 16:05:14 +0900

    1

--- curve_fitting.py
+++ curve_fitting.py
@@ -28,38 +28,12 @@ popt, pcov = curve_fit(func, rgb_values, pH_values, maxfev = 200000)
 a, n1, b, n2, c, n3 = popt
 print("Fitting function:\npH = " + str(a) + " x R^(" + str(n1) + ") + " + str(b) + " x G^(" + str(n2) + ") + " + str(c) + " x B^(" + str(n3) + ")")
 
-# Create figure
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
 # Fitting
 r_fit = np.linspace(min(r_values), max(r_values), 30)
 g_fit = np.linspace(min(g_values), max(g_values), 30)
 b_fit = np.linspace(min(b_values), max(b_values), 30)
 R, G, B = np.meshgrid(r_fit, g_fit, b_fit)
 pH = a * R**n1 + b * G**n2 + c * B**n3
-
-# Scatter plot
-img = ax.scatter(R, G, B, c=pH, cmap='viridis_r')
-
-# Colorbar
-colorbar = fig.colorbar(img, location='left')  # Set position to 'left'
-colorbar.set_label('pH')
-
-# Set labels
-ax.set_xlabel('R')
-ax.set_ylabel('G')
-ax.set_zlabel('B')
-# Color of labels
-ax.xaxis.label.set_color('red')
-ax.yaxis.label.set_color('green')
-ax.zaxis.label.set_color('blue')
-# Color of axes
-ax.tick_params(axis='x', colors='red')
-ax.tick_params(axis='y', colors='green')
-ax.tick_params(axis='z', colors='blue')
-# Show figure
-plt.show()
 
 # Save result
 with open('fitting function.csv', 'w') as f:
